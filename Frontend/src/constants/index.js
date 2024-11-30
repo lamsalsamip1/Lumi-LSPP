e004938588b9ffd4e8a1efea9ef4751813451e7a
NibidaGhimire <nibidaghimire39@gmail.com>
2024-11-30 13:29:32 +0545

    add newsItems to constant

--- Frontend/src/constants/index.js
+++ Frontend/src/constants/index.js
@@ -58,3 +58,38 @@ export const creators = [
       linkedin:"https://www.linkedin.com/in/priti-ranabhat/"
     }
   ]
+
+  export const newsItems = [
+    {
+        title: "BCA Re-entrance Result 2081 - TU",
+        url: "https://edusanjal.com/news/bca-re-entrance-result-tu/",
+        description: "Tribhuvan University Faculty of Humanities and Social Sciences (TUFoHSS) has released the Bachelor of Computer Application (BCA) re-entrance examination results for the academic year 2081.",
+        date: "November 20, 2024",
+        category: "Technology",
+        readTime: "5 min read"
+    },
+    {
+        title: "The British College announces Career Fest 2024",
+        url: "https://www.educatenepal.com/news/detail/the-british-college-announces-career-fest-2024",
+        description: "The British College, in collaboration with Growth Sellers, is hosting Career Fest 2024, connecting job seekers with over 50 leading companies.",
+        date: "November 11, 2024",
+        category: "Career Event",
+        readTime: "7 min read"
+    },
+    {
+        title: "Top 10 IT Colleges in Nepal: IT Courses, Fees, Scholarships",
+        url: "https://meroupdate.com/university/top-10-it-colleges-in-nepal/?utm_source=chatgpt.com",
+        description: "Discover Nepal’s top 10 IT colleges, featuring course offerings, fee structures, scholarship opportunities, and admission processes for aspiring IT professionals.",
+        date: "November 2, 2024",
+        category: "Education",
+        readTime: "5 min read"
+    },
+    {
+        title: "BE/BArch. Entrance Result 2081",
+        url: "https://entrance.ioe.edu.np/notice/download?filename=BEResult2081.pdf&contentType=pdf",
+        description: "Tribhuvan University's Institute of Engineering has released the results for the BE/BArch entrance examinations for the year 2081. Read more to check your results.",
+        date: "September 7, 2024",
+        category: "Technology",
+        readTime: "2 min read"
+    }
+];
