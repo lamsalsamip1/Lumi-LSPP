32c6796a137e1d529d4404a3b7d479b3f04eba21
Samip Lamsal <lamsalsamip@outlook.com>
2024-11-30 23:01:35 +0545

    Final Touches

--- Frontend/src/constants/index.js
+++ Frontend/src/constants/index.js
@@ -1,95 +1,96 @@
-
 export const navLinks = [
-    {
-      id: "news",
-      title: "Latest News",
-    },
-    {
-      id: "aboutus",
-      title: "About Us",
-    },
-    
+  {
+    id: "news",
+    title: "Latest News",
+  },
+  {
+    id: "aboutus",
+    title: "About Us",
+  },
 ];
 
 export const prompts = [
-    {
-      id: "1",
-      title: "Tell me about CSIT courses",
-    },
-    {
-      id: "2",
-      title: "What is the admission process for CSIT at TU?",
-    },
-    {
-      id: "3",
-      title: "Admission for Engineering at TU",
-    }
-    
+  {
+    id: "1",
+    title: "Tell me about Computer Engineering courses.",
+  },
+  {
+    id: "2",
+    title: "What is the admission process for CSIT at TU?",
+  },
+  {
+    id: "3",
+    title:
+      "Can you tell me about foreign university affiliated colleges in Nepal ?",
+  },
 ];
 
-
 export const creators = [
-    {
-      id: "1",
-      name: "Samip Lamsal",
-      role: "",
-      github: "https://github.com/lamsalsamip1",
-      linkedin:"https://www.linkedin.com/in/lamsalsamip1/"
-    },
-    {
-      id: "2",
-      name: "Nibida Ghimire",
-      role: "",
-      github: "https://github.com/NibidaGhimire",
-      linkedin:"https://www.linkedin.com/in/nibida-ghimire/"
-    },
-    {
-      id: "3",
-      name: "Sweta Suwal",
-      role: "",
-      github: "https://github.com/Zdrlfx",
-      linkedin:"https://www.linkedin.com/in/suwalsweta/"
-    },
-    {
-      id: "4",
-      name: "Priti Ranabhat",
-      role: "",
-      github: "https://github.com/pritikaaa",
-      linkedin:"https://www.linkedin.com/in/priti-ranabhat/"
-    }
-  ]
+  {
+    id: "1",
+    name: "Samip Lamsal",
+    role: "",
+    github: "https://github.com/lamsalsamip1",
+    linkedin: "https://www.linkedin.com/in/lamsalsamip1/",
+  },
+  {
+    id: "2",
+    name: "Nibida Ghimire",
+    role: "",
+    github: "https://github.com/NibidaGhimire",
+    linkedin: "https://www.linkedin.com/in/nibida-ghimire/",
+  },
+  {
+    id: "3",
+    name: "Sweta Suwal",
+    role: "",
+    github: "https://github.com/Zdrlfx",
+    linkedin: "https://www.linkedin.com/in/suwalsweta/",
+  },
+  {
+    id: "4",
+    name: "Priti Ranabhat",
+    role: "",
+    github: "https://github.com/pritikaaa",
+    linkedin: "https://www.linkedin.com/in/priti-ranabhat/",
+  },
+];
 
-  export const newsItems = [
-    {
-        title: "BCA Re-entrance Result 2081 - TU",
-        url: "https://edusanjal.com/news/bca-re-entrance-result-tu/",
-        description: "Tribhuvan University Faculty of Humanities and Social Sciences (TUFoHSS) has released the Bachelor of Computer Application (BCA) re-entrance examination results for the academic year 2081.",
-        date: "November 20, 2024",
-        category: "Technology",
-        readTime: "5 min read"
-    },
-    {
-        title: "The British College announces Career Fest 2024",
-        url: "https://www.educatenepal.com/news/detail/the-british-college-announces-career-fest-2024",
-        description: "The British College, in collaboration with Growth Sellers, is hosting Career Fest 2024, connecting job seekers with over 50 leading companies.",
-        date: "November 11, 2024",
-        category: "Career Event",
-        readTime: "7 min read"
-    },
-    {
-        title: "Top 10 IT Colleges in Nepal: IT Courses, Fees, Scholarships",
-        url: "https://meroupdate.com/university/top-10-it-colleges-in-nepal/?utm_source=chatgpt.com",
-        description: "Discover Nepal’s top 10 IT colleges, featuring course offerings, fee structures, scholarship opportunities, and admission processes for aspiring IT professionals.",
-        date: "November 2, 2024",
-        category: "Education",
-        readTime: "5 min read"
-    },
-    {
-        title: "BE/BArch. Entrance Result 2081",
-        url: "https://entrance.ioe.edu.np/notice/download?filename=BEResult2081.pdf&contentType=pdf",
-        description: "Tribhuvan University's Institute of Engineering has released the results for the BE/BArch entrance examinations for the year 2081. Read more to check your results.",
-        date: "September 7, 2024",
-        category: "Technology",
-        readTime: "2 min read"
-    }
+export const newsItems = [
+  {
+    title: "BCA Re-entrance Result 2081 - TU",
+    url: "https://edusanjal.com/news/bca-re-entrance-result-tu/",
+    description:
+      "Tribhuvan University Faculty of Humanities and Social Sciences (TUFoHSS) has released the Bachelor of Computer Application (BCA) re-entrance examination results for the academic year 2081.",
+    date: "November 20, 2024",
+    category: "Technology",
+    readTime: "5 min read",
+  },
+  {
+    title: "The British College announces Career Fest 2024",
+    url: "https://www.educatenepal.com/news/detail/the-british-college-announces-career-fest-2024",
+    description:
+      "The British College, in collaboration with Growth Sellers, is hosting Career Fest 2024, connecting job seekers with over 50 leading companies.",
+    date: "November 11, 2024",
+    category: "Career Event",
+    readTime: "7 min read",
+  },
+  {
+    title: "Top 10 IT Colleges in Nepal: IT Courses, Fees, Scholarships",
+    url: "https://meroupdate.com/university/top-10-it-colleges-in-nepal/?utm_source=chatgpt.com",
+    description:
+      "Discover Nepal’s top 10 IT colleges, featuring course offerings, fee structures, scholarship opportunities, and admission processes for aspiring IT professionals.",
+    date: "November 2, 2024",
+    category: "Education",
+    readTime: "5 min read",
+  },
+  {
+    title: "BE/BArch. Entrance Result 2081",
+    url: "https://entrance.ioe.edu.np/notice/download?filename=BEResult2081.pdf&contentType=pdf",
+    description:
+      "Tribhuvan University's Institute of Engineering has released the results for the BE/BArch entrance examinations for the year 2081. Read more to check your results.",
+    date: "September 7, 2024",
+    category: "Technology",
+    readTime: "2 min read",
+  },
 ];
